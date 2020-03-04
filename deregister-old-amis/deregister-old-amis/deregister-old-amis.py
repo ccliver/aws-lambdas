57244d1f721b82bf5ed1fc07e34b010e8ed79f8c
Carl Cliver <carl@Carls-MacBook-Air-2.local>
2020-03-03 22:40:32 -0500

    Finishing up

--- deregister-old-amis/deregister-old-amis/deregister-old-amis.py
+++ deregister-old-amis/deregister-old-amis/deregister-old-amis.py
@@ -8,7 +8,7 @@ def days_old(date):
     diff = datetime.datetime.now() - parsed
     return diff.days
 
-def deregister_amis():
+def deregister_amis(account_id):
     """Find AMIs with the AMI_TAG tag and if they are older than MAX_DAYS deregister them."""
 
     tag = os.getenv('AMI_TAG')
@@ -17,7 +17,7 @@ def deregister_amis():
 
     for region in regions.split(','):
         ec2 = boto3.resource('ec2', region_name=region)
-        for image in ec2.images.filter(Filters=[{'Name': 'owner-id', 'Values': ['482275948017']}, {'Name': 'tag-key', 'Values': ['Deletable']}]):
+        for image in ec2.images.filter(Filters=[{'Name': 'owner-id', 'Values': [account_id]}, {'Name': 'tag-key', 'Values': [tag]}]):
             image_days_old = days_old(image.creation_date)
             print("Image {}, CreationDate: {} ({} days old)".format(image.id, image.creation_date, image_days_old))
 
@@ -26,7 +26,8 @@ def deregister_amis():
                 image.deregister()
 
 def lambda_handler(event, context):
-    deregister_amis()
+    deregister_amis(context.invoked_function_arn.split(':')[4])
 
 if __name__ == '__main__':
-    deregister_amis()
+    account_id = boto3.client("sts").get_caller_identity()["Account"]
+    deregister_amis(account_id)
